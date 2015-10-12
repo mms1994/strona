5710d2442e8d0c88a94adfdff47dd929bf858ab1
mms1994 <michal.spirala@hotmail.com>
2015-10-12 19:39:03 +0200

    fix

--- samochodubezpieczenieusun.php
+++ samochodubezpieczenieusun.php
@@ -1,13 +1,13 @@
 <?php
 include('template/header.php');
 ?>
-
 <?php
+echo $_POST['ubezpieczenie_ID'];
 $car_ID=$_POST['car_ID'];
 if($_POST['rodzaj']=="polisa") {
     $ubezpieczenie_ID = $_POST['ubezpieczenie_ID'];
 
-    if (unlink('pliki/potwierdzeniaplatnosci/paliwo/' . $car_ID . '_' . $ubezpieczenie_ID . '.pdf')) {
+    if (unlink('pliki/polisy/komunikacyjne/' . $car_ID . '_' . $ubezpieczenie_ID . '.pdf')) {
         echo 'Operacja zakończona pomyślnie.<br />';
     } else {
         echo 'Błąd. Spróbuj ponownie później.<br />';
@@ -16,7 +16,7 @@ if($_POST['rodzaj']=="polisa") {
 if($_POST['rodzaj']=="potwierdzenie") {
     $ubezpieczenie_ID = $_POST['ubezpieczenie_ID'];
 
-    if (unlink('pliki/potwierdzeniaplatnosci/serwis/' . $car_ID . '_' . $ubezpieczenie_ID . '.pdf')) {
+    if (unlink('pliki/potwierdzeniaplatnosci/ubezpieczenie/' . $car_ID . '_' . $ubezpieczenie_ID . '.pdf')) {
         echo 'Operacja zakończona pomyślnie.<br />';
     } else {
         echo 'Błąd. Spróbuj ponownie później.<br />';

a0bb04ef8b9f452cdedd22b61ce1bbd426a2ce29
Michał Spirała <michal.spirala@gmail.com>
2015-10-12 09:46:45 +0200

    dodawanie serwisu

---
+++ serwisdodaj2.php
@@ -0,0 +1,27 @@
+<?php
+include('template/header.php');
+?>
+
+<?php
+
+$car_ID=$_POST['id'];
+$nazwa=$_POST['nazwa'];
+$miejscowosc=$_POST['miejscowosc'];
+$ulica=$_POST['ulica'];
+$numer=$_POST['numer'];
+
+$zapytanie = "INSERT INTO serwis VALUES ('', '$nazwa', '$miejscowosc', '$ulica', '$numer')";
+$wynik = mysql_query($zapytanie);
+
+if ($wynik) {
+    echo 'Wpis dodany prawidłowo<br />';
+    echo '<form method="post" action="samochodserwisuj.php"><input type="hidden" name="id" value='.$car_ID.' />&nbsp;<input type="submit" value="Dalej"/>&nbsp;</form>';
+} else {
+    echo 'Błąd spróbuj ponownie później<a href="samochod.php">POWRÓT</a><br />';
+}
+
+?>
+
+<?php
+include('template/footer.php');
+?>

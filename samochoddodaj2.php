fea068e1ee0e7a03fe1276545bf4f2f28dab5d7f
Michał Spirała <michal.spirala@gmail.com>
2015-10-20 01:56:38 +0200

    początek walidacji php w dodajsamochod

--- samochoddodaj2.php
+++ samochoddodaj2.php
@@ -11,18 +11,26 @@ $przebieg=htmlspecialchars(mysql_real_escape_string($_POST['przebieg']));
 $vin=htmlspecialchars(mysql_real_escape_string($_POST['vin']));
 $nrrej=htmlspecialchars(mysql_real_escape_string($_POST['nrrej']));
 
-$zapytanie = "INSERT INTO cars VALUES ('', '$marka', '$model', '$rocznik', '$przebieg', '$wl', '$vin', '$nrrej', '0')";
-$wynik = mysql_query($zapytanie);
+$valid=true;
+if(!preg_match('[a-zA-Z]', $marka)) $valid=false;
+if(!preg_match('[a-zA-Z]', $model)) $valid=false;
+if($valid) {
+    $zapytanie = "INSERT INTO cars VALUES ('', '$marka', '$model', '$rocznik', '$przebieg', '$wl', '$vin', '$nrrej', '0')";
+    $wynik = mysql_query($zapytanie);
 
-if ($wynik) {
-    echo 'Wpis dodany prawidłowo<br />';
-    echo '<a href="samochod.php">POWRÓT</a><br />';
-} else {
-    if(mysql_errno()==1062)
-        echo 'Podany VIN lub nr rejestracyjny został już użyty!<br />';
-    echo 'Błąd spróbuj ponownie później <a href="samochod.php">POWRÓT</a><br />';
+    if ($wynik) {
+        echo 'Wpis dodany prawidłowo<br />';
+        echo '<a href="samochod.php">POWRÓT</a><br />';
+    } else {
+        if (mysql_errno() == 1062)
+            echo 'Podany VIN lub nr rejestracyjny został już użyty!<br />';
+        echo 'Błąd spróbuj ponownie później <a href="samochod.php">POWRÓT</a><br />';
+    }
 }
-
+else {
+    echo "Formularz został błędnie wypełniony!<br />";
+    echo 'Spróbuj ponownie <a href="samochod.php">POWRÓT</a><br />';
+}
 ?>
 <?php
 include('template/footer.php');

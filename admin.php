b6d0a5b5268c200b0a1f26440d610e2abce3ba0d
Michał <michal.spirala@gmail.com>
2015-11-22 02:05:56 +0100

    tankowania

--- admin.php
+++ admin.php
@@ -46,18 +46,74 @@ if (user::isLogged()) {
         </div>
         <div id="serwisy" style="display: none;">
         <button onclick="div_hide('serwisy'), div_show('serwisy_pokaz')">UKRYJ SERWISY</button>
-        <?php
-        //lista tankowań
-        ?>
         </div>
         <div id="tankowania_pokaz" style="display: ;" >
             <button onclick="div_show('tankowania'), div_hide('tankowania_pokaz')">POKAŻ TANKOWANIA</button>
         </div>
         <div id="tankowania" style="display: none;">
         <button onclick="div_hide('tankowania'), div_show('tankowania_pokaz')">UKRYJ TANKOWANIA</button>
-        <?php
-        //lista serwisowań
-        ?>
+            <table border="1">
+                <tr>
+                    <td>&nbsp;Data&nbsp;</td>
+                    <td>&nbsp;Przebieg&nbsp;</td>
+                    <td>&nbsp;Ilość&nbsp;</td>
+                    <td>&nbsp;Koszt&nbsp;</td>
+                    <td>&nbsp;Dystans&nbsp;</td>
+                    <td>&nbsp;Cena za litr&nbsp;</td>
+                    <td>&nbsp;Id samochodu&nbsp;</td>
+                    <td>&nbsp;Stacja paliw&nbsp;</td>
+                    <td>&nbsp;Potwierdzenie płatności&nbsp;</td>
+                </tr>
+
+                <?php
+                $zapytanie = mysqli_query($mysqli, "SELECT * FROM tankowanie");
+                while ($row = mysqli_fetch_array($zapytanie, MYSQLI_ASSOC)) {
+                    $Data=$row['data'];
+                    $Przebieg=$row['przebieg'];
+                    $Ilosc=$row['ilosc'];
+                    $Koszt=$row['koszt'];
+                    $Dystans=$row['dystans'];
+                    $Cena=$row['cena'];
+                    $car_ID=$row['car_id'];
+                    $Stacja_id=$row['stacja_id'];
+                    $zapytanie2 = mysqli_query($mysqli, "SELECT * FROM stacja WHERE stacja_id='$Stacja_id'");
+                    $row2 = mysqli_fetch_array($zapytanie2, MYSQLI_ASSOC);
+                    $nazwa=$row2['nazwa'];
+                    $miejscowosc=$row2['miejscowosc'];
+                    $ulica=$row2['ulica'];
+                    $numer=$row2['numer'];
+                    $Stacja=$nazwa.', '.$miejscowosc.', '.$ulica.' '.$numer;
+                    $tankowanie_ID=$row['tankowanie_id'];
+                    $pk="pliki/potwierdzeniaplatnosci/paliwo/".$car_ID."_".$tankowanie_ID.".pdf";
+                    ?>
+                    <tr>
+                        <td>&nbsp;<?php echo $Data; ?>&nbsp;</td>
+                        <td>&nbsp;<?php echo $Przebieg; ?>&nbsp;</td>
+                        <td>&nbsp;<?php echo $Ilosc; ?>&nbsp;</td>
+                        <td>&nbsp;<?php echo $Koszt; ?>&nbsp;</td>
+                        <td>&nbsp;<?php echo $Dystans; ?>&nbsp;</td>
+                        <td>&nbsp;<?php echo $Cena; ?>&nbsp;</td>
+                        <td>&nbsp;<?php echo $car_ID; ?>&nbsp;</td>
+                        <td>&nbsp;<?php echo $Stacja; ?>&nbsp;</td>
+                        <td>&nbsp;<?php if(file_exists($pk))
+                            {
+                                echo '<a target="_blank" href="'.$pk.'"><button type="button">&nbsp;PODGLĄD&nbsp;</button></a>&nbsp;';
+                                ?>
+                                <form method="post" onsubmit="return confirm(\'Czy na pewno chcesz skasować?\');" action="samochodparagonusun.php"><input type="hidden" name="tankowanie_ID" value="<?php echo $tankowanie_ID ?>"/><input type="hidden" name="car_ID" value="<?php echo $car_ID; ?>"/><input type="submit" value="Usuń"/></form> <?php
+                            }
+                            else {
+                                ?>
+                                <form method="post" action="samochodparagon.php" enctype="multipart/form-data"><input type="hidden" name="car_ID" value="<?php echo $car_ID; ?>"/><input type="hidden" name="tankowanie_ID" value="<?php echo $tankowanie_ID ?>"/><input type="file" name="plik" accept="application/pdf" required/><input type="submit" value="Wyślij"/> </form>
+                                &nbsp;
+                                <?php
+                            }
+                            ?></td>
+                    </tr>
+                    <?php
+                }
+                ?>
+            </table>
+            Dopuszczalne rozszerzenie plików z paragonem/fakturą to PDF!
         </div>
         <div id="serwisowania_pokaz" style="display: ;" >
             <button onclick="div_show('serwisowania'), div_hide('serwisowania_pokaz')">POKAŻ SERWISOWANIA</button>
@@ -128,9 +184,6 @@ if (user::isLogged()) {
         </table>
         Dopuszczalne rozszerzenie plików z paragonem/fakturą to PDF!
         </div>
-        <?php
-        //lista ubezpieczeń
-        ?>
         <div id="ubezpieczenia_pokaz" style="display: ;" >
             <button onclick="div_show('ubezpieczenia'), div_hide('ubezpieczenia_pokaz')">POKAŻ UBEZPIECZENIA</button>
         </div>

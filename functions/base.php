7f533ebc8104f8593d159d66af4c56c3b512a332
Michał <michal.spirala@gmail.com>
2016-01-28 23:16:38 +0100

    try-catch

--- functions/base.php
+++ functions/base.php
@@ -12,8 +12,12 @@ $cfg['db_name'] = 'strona'; // Nazwa bazy danych
 
 
 // POŁĄCZ Z BAZĄ DANYCH
-$mysqli = new mysqli($cfg['db_server'], $cfg['db_user'], $cfg['db_pass'], $cfg['db_name']);
-
+try {
+    $mysqli = new mysqli($cfg['db_server'], $cfg['db_user'], $cfg['db_pass'], $cfg['db_name']);
+}
+catch (mysqli_sql_exception $e) {
+    echo "Wystapił błąd: ".$e->getMessage();
+}
 if (mysqli_connect_errno()) {
     printf("<p class='error'>Nie udało się połączyc z bazą danych: %s\n</p>", mysqli_connect_error());
     exit();
